48f66b7432156e53b00df5c3bc565250ed26d37c
unknown <maxicenturion10@gmail.com>
2023-05-26 18:27:56 -0300

    Js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,3 @@
-
-
 let nombre;
 nombre = "Juan";
 
@@ -18,7 +16,7 @@ console.log(contador);
 contador = contador + 1; //3
 console.log(contador);
 
-// forma igual // 
+// forma igual //
 
 ++contador; //4
 console.log(contador);
@@ -35,7 +33,8 @@ print "ingrese su edad"
 si edad >= 18
 entonces "Es mayor de edad"
 */
-let edad1;
+
+/* let edad1;
 edad1 = "45";
 
 let edad2;
@@ -48,4 +47,105 @@ if (edad > 18) {
 } else {
     console.log("edad: " + edad);
     console.log("Es menor de edad");
+*/
+
+let dia = 8;
+
+switch (dia) {
+  case 1:
+    console.log("Lunes");
+    break;
+  case 2:
+    console.log("Martes");
+    break;
+  case 3:
+    console.log("Miercoles");
+    break;
+  case 4:
+    console.log("Jueves");
+    break;
+  case 5:
+    console.log("Viernes");
+    break;
+  case 6:
+  case 7:
+    console.log("Fin de semana");
+    break;
+  default:
+    console.log("No es dia valido");
+    break;
 }
+
+/* Estructuras de repetición */
+
+/* while */
+
+let contadorr = 1;
+/*
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+*/
+
+while (contadorr <= 10) {
+  console.log(contadorr);
+  contadorr++;
+}
+
+/* do while*/
+contadorr = 1;
+do {
+  console.log(contadorr);
+  contadorr++;
+} while (contadorr <= 10);
+
+/* for */
+for (let contador2 = 1; contador2 <= 10; contador2++) {
+  console.log(contador2);
+}
+
+// AMBITO (SCOPE) DE UNA VARIABLE - fuera de FOR
+// ES ======>
+// console.log(contador2);
+
+// CUENTA ATRAS
+
+for (let i = 10; i >= 0; i--) {
+  if (i == 0) {
+    console.log("Despegue");
+  }
+  else {
+    console.log("Faltan " + i + " Segundos")
+  }
+}
+
+//IMPRIMIR NUMEROS PARES HASTA 10
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+//IMPRIMIR TABLAS DE MULTIPLICAR
+
+
+
+/* Ejercicios */
+/*
+Se ingresan números hasta que se introduce un cero.
+La computadora muestra el máximo y el mínimo. 
+*/
+let numero, maximo = 0, minimo = 999999;
+do {
+    numero = prompt("Ingrese un número, 0 para terminar");
+    if(numero > maximo) {
+        maximo = numero;
+    }
+    if(numero < minimo && numero != 0) {
+        minimo = numero;
+    }
+} while (numero != 0)
+
+console.log("Máximo: " + maximo);
+console.log("Mínimo: " + minimo);
